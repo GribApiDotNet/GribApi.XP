fa55a005df024bae8fbca5b9292daec706dfce10
0x1mason <etmillin@gmail.com>
2016-04-02 11:44:10 -0400

    Add grib_api png

--- dist/chocolatey/tools/chocolateyinstall.ps1
+++ dist/chocolatey/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'grib-tools'
-$url = 'https://github.com/0x1mason/GribApi.XP/releases/download/v@@VERSION@@/grib-tools-@@VERSION@@-x64.7z'
-$url64 = "https://github.com/0x1mason/GribApi.XP/releases/download/v@@VERSION@@/grib-tools-@@VERSION@@-x86.7z"
+$url64 = 'https://github.com/0x1mason/GribApi.XP/releases/download/v@@VERSION@@/grib-tools-@@VERSION@@-x64.7z'
+$url = "https://github.com/0x1mason/GribApi.XP/releases/download/v@@VERSION@@/grib-tools-@@VERSION@@-x86.7z"
 $installPath = "$env:ChocolateyInstall\bin\grib-tools"
 Write-Verbose $installPath
 Install-ChocolateyZipPackage -PackageName "$packageName" `
