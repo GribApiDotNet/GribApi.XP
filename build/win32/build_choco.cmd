8fcf819dccf972e5755672a87286a689b281f1be
0x1mason <etmillin@gmail.com>
2016-11-26 16:12:18 -0500

    Update vcproj and build automation

--- build/win32/build_choco.cmd
+++ build/win32/build_choco.cmd
@@ -19,11 +19,17 @@ for %%s in (%~dp0..\..\bin\x86\%_CONFIG%\grib_*.exe) do (
  )
 )
 
+rm -rf %~dp0..\..\bin\x64\%_CONFIG%\definitions
+rm -rf %~dp0..\..\bin\x64\%_CONFIG%\samples
+
 xcopy %_GRIB_API%\definitions %~dp0..\..\bin\x64\%_CONFIG%\definitions\ /S /d /I /Q
 xcopy %_GRIB_API%\samples %~dp0..\..\bin\x64\%_CONFIG%\samples\ /S /d /I /Q
 %~dp07zr.exe a %_OUT%/grib-tools-%_VERSION%-x64.7z %_OUT%\grib_*.cmd
 %~dp07zr.exe a %_OUT%/grib-tools-%_VERSION%-x64.7z %~dp0..\..\bin\x64\%_CONFIG%\ -x!.bsc -x!.ilk -x!.map -x!.pdb
 %~dp07zr.exe d %_OUT%/grib-tools-%_VERSION%-x64.7z Release/*.map
+
+rm -rf %~dp0..\..\bin\x86\%_CONFIG%\definitions
+rm -rf %~dp0..\..\bin\x86\%_CONFIG%\samples
 
 xcopy %_GRIB_API%\definitions %~dp0..\..\bin\x86\%_CONFIG%\definitions\ /S /d /I /Q
 xcopy %_GRIB_API%\samples %~dp0..\..\bin\x86\%_CONFIG%\samples\ /S /d /I /Q
